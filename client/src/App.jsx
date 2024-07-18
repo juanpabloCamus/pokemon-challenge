eae8bee60b1f3224593a26741215bcbaa9970b44
Juan Pablo Camus <juanpablo@meowtel.com>
2024-07-18 00:54:15 -0300

    feat: Add last battle modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Box, Typography } from '@mui/material';
 import { PokemonsSelect } from './components/PokemonSelect.jsx';
 import { Arena } from './components/Arena.jsx';
 import { Loader } from './components/Loader.jsx';
+import LastBattlesModal from './components/LastBattlesModal.jsx';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
@@ -24,8 +25,10 @@ function App() {
   }
 
   const handleStartBattle = () => {
+    //Choose a random pokemon
     const randomIndex = Math.floor(Math.random() * pokemons.length);
     let selectRandomPokemon;
+
     if (selectedPokemon1.id === pokemons[randomIndex].id) {
       return handleStartBattle();
     }
@@ -47,37 +50,45 @@ function App() {
   };
 
   const handleSelectPokemon = (pokemon) => {
+    // If the selected pokemon is the same as the first selected pokemon, unselect it
     if (pokemon.id === selectedPokemon2?.id) {
       setSelectedPokemon2(null);
     }
+
+    // Clean the winner and select the first pokemon
+    setWinner(null);
     setSelectedPokemon1(pokemon);
   };
 
   return (
     <div id="app-container">
-      <Typography variant="h4" align="left">
-        Battle of Pokemon
-      </Typography>
-      <Typography variant="h5" marginTop={2} marginBottom={2} align="left">
-        Select your pokemon
-      </Typography>
-      <PokemonsSelect pokemons={pokemons} selectPokemon={handleSelectPokemon} />
-      {winner && (
-        <Box
-          mb={4}
-          textAlign="center"
-          sx={{
-            backgroundColor: '#e4f9fe',
-            border: '1px solid black',
-            borderRadius: '5px',
-            padding: '15px 30px',
-          }}
-        >
-          <Typography variant="h5" align="left">
-            {winner.name} wins!
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box>
+          <Typography variant="h4" align="left">
+            Battle of Pokemon
+          </Typography>
+          <Typography variant="h5" mt={2} mb={2} align="left">
+            Select your pokemon
           </Typography>
         </Box>
-      )}
+        <LastBattlesModal />
+      </Box>
+      <PokemonsSelect pokemons={pokemons} selectPokemon={handleSelectPokemon} />
+      <Box
+        mb={4}
+        textAlign="center"
+        sx={{
+          backgroundColor: '#e4f9fe',
+          border: '1px solid black',
+          borderRadius: '5px',
+          padding: '15px 30px',
+          visibility: winner !== null ? 'visible' : 'hidden',
+        }}
+      >
+        <Typography variant="h5" align="left">
+          {winner?.name} wins!
+        </Typography>
+      </Box>
       <Arena
         pokemon1={selectedPokemon1}
         pokemon2={selectedPokemon2}
